b24b6215318baf76af6ee001c044d64bc8a83534
David John Arnold Jr <david.j.arnold.jr@census.gov>
2024-03-28 13:12:21 -0700

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -4,71 +4,71 @@
 #http://aws.amazon.com/agreement or other written agreement between Customer and either
 #Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.
 
-resource "aws_iam_role" "replication_s3_role" {
-  name = "${var.project_name}-replication-role"
+# resource "aws_iam_role" "replication_s3_role" {
+#   name = "${var.project_name}-replication-role"
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "s3.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-POLICY
-}
+#   assume_role_policy = <<POLICY
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": "sts:AssumeRole",
+#       "Principal": {
+#         "Service": "s3.amazonaws.com"
+#       },
+#       "Effect": "Allow",
+#       "Sid": ""
+#     }
+#   ]
+# }
+# POLICY
+# }
 
-resource "aws_iam_policy" "replication_s3_policy" {
-  name   = "${var.project_name}-replication-policy"
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:GetReplicationConfiguration",
-        "s3:ListBucket"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-        "${aws_s3_bucket.codepipeline_bucket.arn}"
-      ]
-    },
-    {
-      "Action": [
-        "s3:GetObjectVersionForReplication",
-        "s3:GetObjectVersionAcl",
-         "s3:GetObjectVersionTagging"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-        "${aws_s3_bucket.codepipeline_bucket.arn}/*"
-      ]
-    },
-    {
-      "Action": [
-        "s3:ReplicateObject",
-        "s3:ReplicateDelete",
-        "s3:ReplicateTags"
-      ],
-      "Effect": "Allow",
-      "Resource": "${aws_s3_bucket.replication_bucket.arn}/*"
-    }
-  ]
-}
-POLICY
-}
+# resource "aws_iam_policy" "replication_s3_policy" {
+#   name   = "${var.project_name}-replication-policy"
+#   policy = <<POLICY
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": [
+#         "s3:GetReplicationConfiguration",
+#         "s3:ListBucket"
+#       ],
+#       "Effect": "Allow",
+#       "Resource": [
+#         "${aws_s3_bucket.codepipeline_bucket.arn}"
+#       ]
+#     },
+#     {
+#       "Action": [
+#         "s3:GetObjectVersionForReplication",
+#         "s3:GetObjectVersionAcl",
+#          "s3:GetObjectVersionTagging"
+#       ],
+#       "Effect": "Allow",
+#       "Resource": [
+#         "${aws_s3_bucket.codepipeline_bucket.arn}/*"
+#       ]
+#     },
+#     {
+#       "Action": [
+#         "s3:ReplicateObject",
+#         "s3:ReplicateDelete",
+#         "s3:ReplicateTags"
+#       ],
+#       "Effect": "Allow",
+#       "Resource": "${aws_s3_bucket.replication_bucket.arn}/*"
+#     }
+#   ]
+# }
+# POLICY
+# }
 
-resource "aws_iam_role_policy_attachment" "replication_s3_role_attach" {
-  role       = aws_iam_role.replication_s3_role.name
-  policy_arn = aws_iam_policy.replication_s3_policy.arn
-}
+# resource "aws_iam_role_policy_attachment" "replication_s3_role_attach" {
+#   role       = aws_iam_role.replication_s3_role.name
+#   policy_arn = aws_iam_policy.replication_s3_policy.arn
+# }
 
 # #Replication bucket
 # resource "aws_s3_bucket" "replication_bucket" {
