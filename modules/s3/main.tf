6890460fdac8b14fc93819d00b4e2363c68ed0e3
David John Arnold Jr <david.j.arnold.jr@census.gov>
2024-03-28 12:43:18 -0700

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -226,28 +226,28 @@ resource "aws_s3_bucket_logging" "codepipeline_bucket_logging" {
   target_prefix = "log/"
 }
 
-resource "aws_s3_bucket_replication_configuration" "replication_config" {
-  #provider = aws.replication
-  # Must have bucket versioning enabled first
-  depends_on = [aws_s3_bucket_versioning.codepipeline_bucket_versioning]
+# resource "aws_s3_bucket_replication_configuration" "replication_config" {
+#   #provider = aws.replication
+#   # Must have bucket versioning enabled first
+#   depends_on = [aws_s3_bucket_versioning.codepipeline_bucket_versioning]
 
-  role   = aws_iam_role.replication_s3_role.arn
-  bucket = aws_s3_bucket.codepipeline_bucket.id
+#   role   = aws_iam_role.replication_s3_role.arn
+#   bucket = aws_s3_bucket.codepipeline_bucket.id
 
-  rule {
-    id = "${var.project_name}-replication-rule"
+#   rule {
+#     id = "${var.project_name}-replication-rule"
 
-    filter {}
+#     filter {}
 
-    delete_marker_replication {
-      status = "Enabled"
-    }
+#     delete_marker_replication {
+#       status = "Enabled"
+#     }
 
-    status = "Enabled"
+#     status = "Enabled"
 
-    destination {
-      bucket        = aws_s3_bucket.replication_bucket.arn
-      storage_class = "STANDARD"
-    }
-  }
-}
+#     destination {
+#       bucket        = aws_s3_bucket.replication_bucket.arn
+#       storage_class = "STANDARD"
+#     }
+#   }
+# }
