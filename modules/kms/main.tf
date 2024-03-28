ead051f54abec5094c61d14fba005deb518f9aa6
David John Arnold Jr <david.j.arnold.jr@census.gov>
2024-03-28 14:40:09 -0700

    Update main.tf

--- modules/kms/main.tf
+++ modules/kms/main.tf
@@ -11,28 +11,28 @@ locals {
 resource "aws_kms_key" "encryption_key" {
   description             = "This key is used to encrypt bucket objects"
   deletion_window_in_days = 10
-  policy                  = data.aws_iam_policy_document.kms_key_policy_doc.json
+  # policy                  = data.aws_iam_policy_document.kms_key_policy_doc.json
   enable_key_rotation     = true
   tags                    = var.tags
 }
 
 data "aws_iam_policy_document" "kms_key_policy_doc" {
-  statement {
-    sid       = "Enable IAM User Permissions"
-    effect    = "Allow"
-    actions   = ["kms:*"]
-    #checkov:skip=CKV_AWS_111:Without this statement, KMS key cannot be managed by root
-    #checkov:skip=CKV_AWS_109:Without this statement, KMS key cannot be managed by root
-    resources = ["*"]
+  # statement {
+  #   sid       = "Enable IAM User Permissions"
+  #   effect    = "Allow"
+  #   actions   = ["kms:*"]
+  #   #checkov:skip=CKV_AWS_111:Without this statement, KMS key cannot be managed by root
+  #   #checkov:skip=CKV_AWS_109:Without this statement, KMS key cannot be managed by root
+  #   resources = ["*"]
 
-    principals {
-      type        = "AWS"
-      identifiers = [
-        "arn:aws:iam::${local.account_id}:root",
-        data.aws_caller_identity.current.arn
-      ]
-    }
-  }
+  #   principals {
+  #     type        = "AWS"
+  #     identifiers = [
+  #       "arn:aws:iam::${local.account_id}:root",
+  #       data.aws_caller_identity.current.arn
+  #     ]
+  #   }
+  # }
 
   statement {
     sid       = "Allow access for Key Administrators"
